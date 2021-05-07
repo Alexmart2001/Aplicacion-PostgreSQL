528e6cb8026a43d7bcf8b9bb6a0c739a47adc842
Alexander Martinez <83377810+Alexmart2001@users.noreply.github.com>
2021-05-06 20:36:45 -0500

    Add files via upload

---
+++ src/main/java/co/edu/usbbog/abd/postgresql_spring_boot/PostgreSqlSpringBootApplication.java
@@ -0,0 +1,13 @@
+package co.edu.usbbog.abd.postgresql_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostgreSqlSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PostgreSqlSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/co/edu/usbbog/abd/postgresql_spring_boot/PostgreSqlSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.usbbog.abd.postgresql_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostgreSqlSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
